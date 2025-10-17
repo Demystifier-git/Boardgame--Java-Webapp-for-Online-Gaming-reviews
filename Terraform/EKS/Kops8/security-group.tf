4116653b51e5a36348b171accbe2769b58fbfb7e
devops4sure <devops4sure@gmail.com>
2025-10-17 09:05:02 -0700

    new changes

--- Terraform/EKS/Kops8/security-group.tf
+++ Terraform/EKS/Kops8/security-group.tf
@@ -22,10 +22,22 @@ resource "aws_security_group" "alb_sg" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] 
   }
+
 
   tags = {
     Name = "alb-sg"
   }
 }
+
+resource "aws_security_group_rule" "alb_outbound_https" {
+  type              = "egress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.alb_sg.id
+  description       = "Allow outbound HTTPS access to internet"
+}
+
