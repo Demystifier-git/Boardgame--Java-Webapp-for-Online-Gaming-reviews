1cf9c65141917f2019a5a49a7f325153102055c2
devops4sure <devops4sure@gmail.com>
2025-10-17 23:44:42 -0700

    latest update

--- Terraform/EKS/Kops8/LoadBalancer-sg.tf
+++ Terraform/EKS/Kops8/LoadBalancer-sg.tf
@@ -1,5 +1,5 @@
-# 2. Security group for ALB
-resource "aws_albb" "alb_sg" {
+# Security group for ALB
+resource "aws_security_group" "alb_sg" {
   name        = "alb-sg"
   description = "Allow HTTPS from anywhere"
   vpc_id      = data.aws_vpc.existing.id
@@ -19,3 +19,4 @@ resource "aws_albb" "alb_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
