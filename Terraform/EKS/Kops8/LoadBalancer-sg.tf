2faa903178d43803fe0acf420f87bc8d34dd14d0
devops4sure <devops4sure@gmail.com>
2025-10-21 03:49:22 -0700

    latest update

---
+++ Terraform/EKS/Kops8/LoadBalancer-sg.tf
@@ -0,0 +1,22 @@
+# Security group for ALB
+resource "aws_security_group" "alb_sg" {
+  name        = "alb-sg"
+  description = "Allow HTTPS from anywhere"
+  vpc_id      = data.aws_vpc.existing.id
+
+  ingress {
+    description = "HTTPS from anywhere"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
